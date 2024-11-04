fd4f3d58d05a7bffff1f875a0157f785f248f7fb
Fabio Slika Stella <gedankenn@gmail.com>
2024-11-04 14:53:19 -0300

    fix adc submodule, wrong folders

--- adc/adc/adc.h
+++
@@ -1,9 +0,0 @@
-#include "esp_adc/adc_oneshot.h"
-
-void adc1_init(void);
-void adc1_channel_init(int channel);
-int adc1_read(int channel);
-void adc2_init(void);
-void adc2_channel_init(int channel);
-int adc2_read(int channel);
-
